3c6646999f5fb1cbf6795472572bbfd3ff50529c
Grace Farnsworth <gmfarnz@byu.edu>
2025-03-14 14:07:12 -0600

    initial commit

---
+++ backend/WaterProject.API/Data/WaterDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore;
+
+namespace WaterProject.API.Data
+{
+    public class WaterDbContext: DbContext
+    {
+        public WaterDbContext(DbContextOptions<WaterDbContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Project> Projects { get; set; }
+    }
+}
---
+++ frontend/src/types/Project.ts
@@ -0,0 +1,9 @@
+export interface Project {
+  projectId: number;
+  projectName: string;
+  projectType: string;
+  projectRegionalProgram: string;
+  projectImpact: number;
+  ProjectPhase: string;
+  projectFunctionalityStatus: string;
+}
